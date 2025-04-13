47035b71d5edb69a18b42baccc224143ffb5450a
Sheila Comar Varela <sheilacomar@gmail.com>
2025-04-13 17:42:18 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,20 @@
 import mysql.connector
-import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def conectar():
     try:
-        return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="remedio_em_casa_db"
+         return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
         )
-    except mysql.connector.Error as err:
-        st.error(f"Erro ao conectar com o banco de dados: {err}")
+         
+    except mysql.connector.Error as erro:
+        print("❌ Erro ao conectar no banco de dados:")
+        print(erro)
         return None
